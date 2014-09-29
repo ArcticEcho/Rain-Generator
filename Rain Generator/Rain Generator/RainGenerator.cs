7ab8d5d5a1f79b5ae396b54ee23030e4f3bb0580
Sam <s.kamber8@gmail.com>
2014-09-30 00:48:59 +0100

    Removed "wind".

--- Rain Generator/Rain Generator/RainGenerator.cs
+++ Rain Generator/Rain Generator/RainGenerator.cs
@@ -17,10 +17,6 @@ namespace RainGenerator
 		private float singleDropDuration;
 		private int currentDropDuration;
 		private double amplitude;
-		private bool addWind;
-		private bool addedWind = true;
-		private double totalWindDuration;
-		private int windDuration;
 		private int ii;
 
 
@@ -46,45 +42,18 @@ namespace RainGenerator
 					singleDropDuration = sampleRate / dropFreq;                  // Samples for one full wave at specified frequency (i.e., sample count from peak to peak).
 					totalDropDuration = r.Next(1, 7) * singleDropDuration;       // Total number of oscillations ("full waves") long this drop will be drop.
 					amplitude = r.NextDouble();                                  // Choose drop "loudness".
-
-					if (addedWind)
-					{
-						addWind = r.NextDouble() < 1 / (10.0 * sampleRate); // Determine whether or not to add wind.
-						totalWindDuration = r.NextDouble();                 // 0.1 = 10 secs, 0.5 = 2 secs, 1 = 1 sec etc..
-
-						if (addWind)
-						{
-							addedWind = false;
-						}
-					}
 				}
 
-				if (addWind)
-				{
-					samples[i] += (float)(r.NextDouble() + (r.NextDouble() * (Math.Sin(((Math.PI * 2 * totalWindDuration) / sampleRate) * ii)))) * 0.1f;
-
-					windDuration++;
-					ii++;
-
-					if (windDuration >= (1 / totalWindDuration) * sampleRate)
-					{
-						ii = 0;
-						windDuration = 0;
-						addedWind = true;
-						addWind = false;
-					}
-				}
-				else
-				{
-					samples[i] += (float)(GetBackgroundNoise(i, dropFreq) * 0.1);
-				}
+				// Add sound of distant rain.
+				samples[i] += (float)(GetBackgroundNoise(i, dropFreq) * 0.1);
 
 				if (addDrop)
 				{
+					// Add drop.
 					samples[i] += (float)(amplitude * Math.Sin(((Math.PI * 2 * dropFreq) / sampleRate) * i) * 4);
 
+					// Soften drop.
 					samples[i] *= currentDropDuration / totalDropDuration;
-
 					samples[i] *= 1 - currentDropDuration / totalDropDuration;
 
 					addedDrop = false;
@@ -97,8 +66,6 @@ namespace RainGenerator
 						currentDropDuration = 0;
 					}
 				}
-
-				samples[i] *= 0.2f;
 			}
 
 			return samples;
